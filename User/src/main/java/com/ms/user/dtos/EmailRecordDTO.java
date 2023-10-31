d56f522a0e2b80762fe86c8594dc5397e4ae258a
Jânio <janioofi@gmail.com>
2023-10-30 21:14:08 -0300

    Criando producer

---
+++ User/src/main/java/com/ms/user/dtos/EmailRecordDTO.java
@@ -0,0 +1,9 @@
+package com.ms.user.dtos;
+
+import java.util.UUID;
+
+public record EmailRecordDTO(UUID userId,
+                             String emailTo,
+                             String subject,
+                             String text) {
+}
--- User/src/main/java/com/ms/user/services/UserService.java
+++ User/src/main/java/com/ms/user/services/UserService.java
@@ -1,6 +1,7 @@
 package com.ms.user.services;
 
 import com.ms.user.models.UserModel;
+import com.ms.user.producer.UserProducer;
 import com.ms.user.repositories.UserRepository;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
@@ -11,9 +12,12 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository repository;
+    private final UserProducer producer;
 
     @Transactional
     public UserModel save(@Valid UserModel userModel){
-        return repository.save(userModel);
+        userModel = repository.save(userModel);
+        producer.publishMessageEmail(userModel);
+        return userModel;
     }
 }
